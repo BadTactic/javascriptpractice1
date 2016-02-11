994ea53a8163385cc9be782fb59b2050234df802
BadTactic <student@epicodus.com>
2016-02-10 19:27:56 -0800

    built a clickable reveal of gary busey, also added a sliding otter

---
+++ javascript/js.js
@@ -0,0 +1,12 @@
+$(function() {
+  $(".clickable").click(function() {
+    $("#garyshown").toggle();
+    $("#garyhidden").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable1").click(function() {
+    $("#otter").slideToggle();
+  });
+});
